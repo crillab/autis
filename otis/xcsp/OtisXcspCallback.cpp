8cf9919e8d4e42cfe8fb057abf8d2e6009d669bd
Romain Wallon <romain.wallon@univ-artois.fr>
2022-10-24 22:51:48 +0200

    :sparkles: Intension constraints.

--- otis/xcsp/OtisXcspCallback.cpp
+++ otis/xcsp/OtisXcspCallback.cpp
@@ -28,6 +28,7 @@
 */
 
 #include "OtisXcspCallback.hpp"
+#include "../../../libs/universe/universe/include/csp/intension/UniverseJavaIntensionConstraintFactory.hpp"
 
 using namespace XCSP3Core;
 
@@ -47,8 +48,7 @@ namespace Otis {
     }
 
     void OtisXcspCallback::buildConstraintIntension(string id, XCSP3Core::Tree *tree) {
-        //TODO
-        XCSP3CoreCallbacks::buildConstraintIntension(id, tree);
+        solver->addIntension(createIntension(tree->root));
     }
 
     void OtisXcspCallback::buildObjectiveMaximize(XCSP3Core::ExpressionObjective type, vector<XVariable *> &list,
@@ -146,5 +146,153 @@ namespace Otis {
         //XCSP3CoreCallbacks::endVariableArray();
     }
 
+    Universe::IUniverseIntensionConstraint *OtisXcspCallback::createIntension(Node *node) {
+        if (node->type == ExpressionType::ODECIMAL) {
+            return Universe::UniverseJavaIntensionConstraintFactory::constant((long) ((NodeConstant *) node)->val);
+        }
+
+        if (node->type == ExpressionType::OVAR) {
+            return Universe::UniverseJavaIntensionConstraintFactory::variable(((NodeVariable *) node)->var);
+        }
+
+        switch (node->type) {
+            case ExpressionType::OABS: {
+                auto child = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::abs(child);
+            }
+            case ExpressionType::OADD: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::add(constrs);
+            }
+            case ExpressionType::ODIST: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::dist(child1, child2);
+            }
+            case ExpressionType::ODIV: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::div(child1, child2);
+            }
+            case ExpressionType::OMAX: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::max(constrs);
+            }
+            case ExpressionType::OMIN: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::min(constrs);
+            }
+            case ExpressionType::OMOD: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::mod(child1, child2);
+            }
+            case ExpressionType::OMUL: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::mult(constrs);
+            }
+            case ExpressionType::ONEG: {
+                auto child = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::neg(child);
+            }
+            case ExpressionType::OPOW: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::pow(child1, child2);
+            }
+            case ExpressionType::OSQR: {
+                auto child = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::sqr(child);
+            }
+            case ExpressionType::OSUB: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::sub(child1, child2);
+            }
+            case ExpressionType::OIFF: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::equiv(constrs);
+            }
+            case ExpressionType::OAND: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::conjunction(constrs);
+            }
+            case ExpressionType::OIMP: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::impl(child1, child2);
+            }
+            case ExpressionType::ONOT: {
+                auto child = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::negation(child);
+            }
+            case ExpressionType::OOR: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::disjunction(constrs);
+            }
+            case ExpressionType::OXOR: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::parity(constrs);
+            }
+            case ExpressionType::OEQ: {
+                std::vector<Universe::IUniverseIntensionConstraint*> constrs;
+                for (auto child : node->parameters) {
+                    constrs.push_back(createIntension(child));
+                }
+                return Universe::UniverseJavaIntensionConstraintFactory::eq(constrs);
+            }
+            case ExpressionType::OGE: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::ge(child1, child2);
+            }
+            case ExpressionType::OGT: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::gt(child1, child2);
+            }
+            case ExpressionType::OLE: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::le(child1, child2);
+            }
+            case ExpressionType::OLT: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::lt(child1, child2);
+            }
+            case ExpressionType::ONE: {
+                auto child1 = createIntension(node->parameters[0]);
+                auto child2 = createIntension(node->parameters[0]);
+                return Universe::UniverseJavaIntensionConstraintFactory::neq(child1, child2);
+            }
+            default:
+                throw std::runtime_error("No opertator");
+        }
+    }
 
 } // Otis
