b3a51b6e34ab9eeece6c139a698472ee7bf35ce8
Romain Wallon <romain.wallon@univ-artois.fr>
2022-11-13 23:14:09 +0100

    :art: Some improvements

--- autis/xcsp/AutisXcspParserAdapter.cpp
+++ autis/xcsp/AutisXcspParserAdapter.cpp
@@ -1,51 +1,80 @@
 /******************************************************************************
  * AUTIS, a library for parsing combinatorial problems.                       *
- * Copyright (c) 2022 - Exakis Nelite, Univ Artois & CNRS.                    *
+ * Copyright (c) 2022 - Univ Artois & CNRS & Exakis Nelite.                   *
  * All rights reserved.                                                       *
  *                                                                            *
- * This library is free software; you can redistribute it andor               *
- * modify it under the terms of the GNU Lesser General Public                 *
- * License as published by the Free Software Foundation; either               *
- * version 3 of the License, or (at your option) any later version.         *
+ * This library is free software; you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation; either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
  *                                                                            *
- * This library is distributed in the hope that it will be useful,            *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ * This library is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE.                                       *
  * See the GNU Lesser General Public License for more details.                *
  *                                                                            *
  * You should have received a copy of the GNU Lesser General Public           *
  * License along with this library.                                           *
- * If not, see http//:www.gnu.org/licenses.                                   *
+ * If not, see http://www.gnu.org/licenses.                                   *
  ******************************************************************************/
 
 /**
-* @file AutisXcspParserAdapter.cpp
-* @brief
-* @author Thibault Falque
-* @author Romain Wallon
-* @version 0.1.0
-* @date 19/09/2022
-* @copyright Copyright (c) 2022 Exakis Nelite, Univ Artois & CNRS All rights reserved.
-* @license GNU LGPL 3
-*/
+ * @file AutisXCSPParserAdapter.cpp
+ * @brief Defines a parser adapting the callback defined in XCSP3-CPP-Parser.
+ * @author Thibault Falque
+ * @author Romain Wallon
+ * @date 19/09/2022
+ * @copyright Copyright (c) 2022 - Univ Artois & CNRS & Exakis Nelite.
+ * @license This project is released under the GNU LGPL3 License.
+ */
 
-
-#include "AutisXcspParserAdapter.hpp"
+#include "../../libs/universe/universe/include/csp/UniverseJavaCspSolver.hpp"
 #include "../../libs/xcsp3-cpp-parser/include/XCSP3CoreParser.h"
-#include "AutisXcspCallback.hpp"
 
-namespace Autis {
-    AutisXCSPParserAdapter::AutisXCSPParserAdapter(Scanner &scanner, Universe::IUniverseCspSolver* solver) : AbstractParser(scanner,
-                                                                                                                            solver) {}
-    void AutisXCSPParserAdapter::parse() {
-        AutisXcspCallback cb(getConcreteSolver());
-        XCSP3Core::XCSP3CoreParser parser(&cb);
+#include "AutisXcspCallback.hpp"
+#include "AutisJavaXcspCallback.hpp"
+#include "AutisNativeXcspCallback.hpp"
+#include "AutisXcspParserAdapter.hpp"
+
+using namespace Autis;
+using namespace Universe;
+using namespace XCSP3Core;
+
+AutisXCSPParserAdapter::AutisXCSPParserAdapter(Scanner &scanner,
+                                               IUniverseCspSolver *solver, XCSP3CoreCallbacks *callback) :
+        AbstractParser(scanner, solver),
+        callback(callback) {
+    // Nothing to do: everything is already initialized.
+}
+
+void AutisXCSPParserAdapter::parse() {
+    if (callback == nullptr) {
+        // Inferring the most appropriate callback to use.
+        AutisXcspCallback *cb = getCallback();
+        XCSP3CoreParser parser(cb);
+        parser.parse(scanner.getInput());
+        delete cb;
+
+    } else {
+        // Using the specified callback to parse the input.
+        XCSP3CoreParser parser(callback);
         parser.parse(scanner.getInput());
     }
+}
 
-    Universe::IUniverseCspSolver* AutisXCSPParserAdapter::getConcreteSolver() {
-        return dynamic_cast<Universe::IUniverseCspSolver*>(AbstractParser::getConcreteSolver());
+Autis::AutisXcspCallback *AutisXCSPParserAdapter::getCallback() {
+    auto concreteSolver = getConcreteSolver();
+    auto javaSolver = dynamic_cast<UniverseJavaCspSolver *>(concreteSolver);
+
+    if (javaSolver == nullptr) {
+        // The solver is not a Java solver: using a native callback.
+        return new AutisNativeXcspCallback(concreteSolver);
     }
 
+    // The solver is a Java solver: using a dedicated callback.
+    return new AutisJavaXcspCallback(javaSolver);
+}
 
-} // Autis
+IUniverseCspSolver *AutisXCSPParserAdapter::getConcreteSolver() {
+    return dynamic_cast<IUniverseCspSolver *>(AbstractParser::getConcreteSolver());
+}
--- autis/xcsp/AutisXcspParserAdapter.hpp
+++ autis/xcsp/AutisXcspParserAdapter.hpp
@@ -1,57 +1,91 @@
 /******************************************************************************
  * AUTIS, a library for parsing combinatorial problems.                       *
- * Copyright (c) 2022 - Exakis Nelite, Univ Artois & CNRS.                    *
+ * Copyright (c) 2022 - Univ Artois & CNRS & Exakis Nelite.                   *
  * All rights reserved.                                                       *
  *                                                                            *
- * This library is free software; you can redistribute it andor               *
- * modify it under the terms of the GNU Lesser General Public                 *
- * License as published by the Free Software Foundation; either               *
- * version 3 of the License, or (at your option) any later version.         *
+ * This library is free software; you can redistribute it and/or modify it    *
+ * under the terms of the GNU Lesser General Public License as published by   *
+ * the Free Software Foundation; either version 3 of the License, or (at your *
+ * option) any later version.                                                 *
  *                                                                            *
- * This library is distributed in the hope that it will be useful,            *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                       *
+ * This library is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+ * or FITNESS FOR A PARTICULAR PURPOSE.                                       *
  * See the GNU Lesser General Public License for more details.                *
  *                                                                            *
  * You should have received a copy of the GNU Lesser General Public           *
  * License along with this library.                                           *
- * If not, see http//:www.gnu.org/licenses.                                   *
+ * If not, see http://www.gnu.org/licenses.                                   *
  ******************************************************************************/
 
 /**
-* @file AutisXcspParserAdapter.hpp
-* @brief
-* @author Thibault Falque
-* @author Romain Wallon
-* @version 0.1.0
-* @date 19/09/2022
-* @copyright Copyright (c) 2022 Exakis Nelite, Univ Artois & CNRS All rights reserved.
-* @license GNU LGPL 3
-*/
-
+ * @file AutisXCSPParserAdapter.hpp
+ * @brief Defines a parser adapting the callback defined in XCSP3-CPP-Parser.
+ * @author Thibault Falque
+ * @author Romain Wallon
+ * @date 19/09/2022
+ * @copyright Copyright (c) 2022 - Univ Artois & CNRS & Exakis Nelite.
+ * @license This project is released under the GNU LGPL3 License.
+ */
 
 #ifndef AUTIS_AUTISXCSPPARSERADAPTER_HPP
 #define AUTIS_AUTISXCSPPARSERADAPTER_HPP
 
-#include "../core/AbstractParser.hpp"
 #include "../../libs/universe/universe/include/csp/IUniverseCspSolver.hpp"
+
+#include "../core/AbstractParser.hpp"
 
 namespace Autis {
 
-/**
-* @class AutisXCSPParserAdapter
-* @brief 
-*/
-    class AutisXCSPParserAdapter: public AbstractParser {
-    public:
-        AutisXCSPParserAdapter(Scanner &scanner, Universe::IUniverseCspSolver* listener);
+    /**
+     * The AutisXCSPParserAdapter class defines an adapter for the callback interface
+     * defined in XCSP3-CPP-Parser, to use it as an AbstractParser.
+     */
+    class AutisXCSPParserAdapter : public AbstractParser {
 
+    private:
+
+        /**
+         * The callback to use when parsing the input instance.
+         */
+        XCSP3Core::XCSP3CoreCallbacks *callback;
+
+    public:
+
+        /**
+         * Creates a new AutisXCSPParserAdapter.
+         *
+         * @param scanner The scanner to use to read the input instance.
+         * @param solver The solver to feed while parsing the instance.
+         * @param callback The callback to use when parsing the input instance.
+         */
+        AutisXCSPParserAdapter(Autis::Scanner &scanner, Universe::IUniverseCspSolver *solver,
+                               XCSP3Core::XCSP3CoreCallbacks *callback = nullptr);
+
+        /**
+         * Parses the input instance, and feeds the associated solver with the read constraints.
+         */
         void parse() override;
 
     protected:
-        Universe::IUniverseCspSolver*getConcreteSolver() override;
+
+        /**
+         * Gives the callback implementation that matches the actual type of the
+         * solver to feed.
+         *
+         * @return The callback to use when parsing the input instance.
+         */
+        virtual Autis::AutisXcspCallback *getCallback();
+
+        /**
+         * Gives the CSP solver to feed while parsing.
+         *
+         * @return The CSP solver to feed.
+         */
+        Universe::IUniverseCspSolver *getConcreteSolver() override;
+
     };
 
-} // Autis
+}
 
-#endif //AUTIS_AUTISXCSPPARSERADAPTER_HPP
+#endif
